c4cc7e8e8cc66753bf20ee5acfbd9a2a530754c2
Kelly Bruyninx <kelzoe@skynet.be>
2021-02-12 16:32:26 +0100

    fin integration template et img

--- WebAppGestionZoo/App_Start/BundleConfig.cs
+++ WebAppGestionZoo/App_Start/BundleConfig.cs
@@ -8,25 +8,7 @@ namespace WebAppGestionZoo
         // Pour plus d'informations sur le regroupement, visitez https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            // bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-            //           "~/Scripts/jquery-{version}.js"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-            //           "~/Scripts/jquery.validate*"));
-
-            // Utilisez la version de développement de Modernizr pour le développement et l'apprentissage. Puis, une fois
-            // prêt pour la production, utilisez l'outil de génération à l'adresse https://modernizr.com pour sélectionner uniquement les tests dont vous avez besoin.
-            // bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-            //            "~/Scripts/modernizr-*"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-            //         "~/Scripts/bootstrap.js"));
-
-            // bundles.Add(new StyleBundle("~/Content/css").Include(
-            //          "~/Content/bootstrap.css",
-            //          "~/Content/site.css"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include
+             bundles.Add(new StyleBundle("~/Content/css").Include
                 (
                         "~/Content/bootstrap.css",
                         "~/css/style.css"
@@ -34,7 +16,7 @@ namespace WebAppGestionZoo
 
              bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                       "~/Scripts/jquery-{version}.js"
-                     // "~/Scripts/jquery-1.11.1.min.js"
+                   
                 ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryFlexiSel").Include(
