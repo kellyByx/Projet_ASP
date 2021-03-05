6f67d881f3b8cd26f31f56ca4cdca99d65aecc66
Kelly Bruyninx <kelzoe@skynet.be>
2021-03-05 15:50:51 +0100

    suite soigneur

--- DBZoo/dbo/ProcéduresStockées/SP_Soigneur_insert.sql
+++ DBZoo/dbo/ProcéduresStockées/SP_Soigneur_insert.sql
@@ -1,20 +1,20 @@
-﻿--CREATE PROCEDURE [dbo].[SP_Soigneur_insert]
+﻿CREATE PROCEDURE [dbo].[SP_Soigneur_insert]
     
---    @nom            VARCHAR (50),  
---    @prenom         VARCHAR (50),   
---    @specialisation VARCHAR (50),   
---    @email          NVARCHAR (256), 
---    @login          NVARCHAR (32),  
---    @password       VARBINARY (32), 
---    @telephone      NVARCHAR (16),  
---    @photo          NVARCHAR (250)
+   @nom            VARCHAR (50),  
+    @prenom         VARCHAR (50),   
+   @specialisation VARCHAR (50),   
+    @email          NVARCHAR (256), 
+    @login          NVARCHAR (32),  
+    @password       VARBINARY (32), 
+    @telephone      NVARCHAR (16),  
+    @photo          NVARCHAR (250)
        
---AS
---DECLARE @idSoigneur INTEGER, @salt CHAR(8)
---	SET @salt = [dbo].SF_GenerateSalt()
---	INSERT INTO[Soigneur]([Nom], [Prenom], [Specialisation], [Email], [Login], [Password],[Telephone], [Photo],[Salt])
---		   VALUES (@nom, @prenom, @specialisation, @email, @login, dbo.SF_EncryptedPassword(@password, @salt),@telephone, @photo,@salt)
---	SET @idSoigneur = @@IDENTITY
+AS
+DECLARE @idSoigneur INTEGER, @salt CHAR(8)
+	SET @salt = [dbo].SF_GenerateSalt()
+	INSERT INTO[Soigneur]([Nom], [Prenom], [Specialisation], [Email], [Login], [Password],[Telephone], [Photo],[Salt])
+		   VALUES (@nom, @prenom, @specialisation, @email, @login, dbo.SF_EncryptedPassword(@password, @salt),@telephone, @photo,@salt)
+	SET @idSoigneur = @@IDENTITY
 
  --  ou  DECLARE @idSoigneur INT, @salt CHAR(8)
 
